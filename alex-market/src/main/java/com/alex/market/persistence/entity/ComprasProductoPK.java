787cc3c6df9410a913fd8df4203e65f4f7a683bd
Alex <30849299+Xandermelendez@users.noreply.github.com>
2021-01-12 20:18:22 -0600

    Commit 1

---
+++ alex-market/src/main/java/com/alex/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,31 @@
+package com.alex.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_Producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
